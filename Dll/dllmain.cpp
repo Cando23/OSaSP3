59c70094cacb07762bcbb3c55bf864dbbe5993ef
Roman Polishchuk <rmn.polishchuk@gmail.com>
2021-11-13 16:40:47 +0300

    add simple Dll for import testing

---
+++ Dll/dllmain.cpp
@@ -0,0 +1,7 @@
+﻿// dllmain.cpp : Определяет точку входа для приложения DLL.
+#include "pch.h"
+#include <iostream>
+#define DllExport __declspec(dllexport)
+extern "C" DllExport void __cdecl DoSomething() {
+    std::cout << "Hi";
+}
