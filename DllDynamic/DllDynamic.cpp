8d5fe4173d526626fc8ee66eddb05ea31921638c
Roman Polishchuk <rmn.polishchuk@gmail.com>
2021-11-13 16:41:56 +0300

    add Dll dynamic import solution

---
+++ DllDynamic/DllDynamic.cpp
@@ -0,0 +1,19 @@
+﻿#include "Windows.h"
+#include "iostream"
+typedef void __cdecl DoFunc();
+int main()
+{
+	HINSTANCE lib;
+	lib = LoadLibrary(L"Dll.dll");
+	if (lib != NULL) {
+		DoFunc* proc = (DoFunc*)GetProcAddress(lib, "DoSomething");
+		if (NULL != proc) {
+			proc();
+		}
+		else {
+			std::cout << "NO";
+		}
+		FreeLibrary(lib);
+	}
+	return 0;
+}
